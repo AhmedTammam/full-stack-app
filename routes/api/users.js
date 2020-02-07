8eab7270d73491bcf450f3fd5c8c30e5b5665efa
Ahmed Tammam <tamam65@gmail.com>
2020-02-07 03:40:45 +0200

    fix code review issues

--- routes/api/users.js
+++ routes/api/users.js
@@ -12,30 +12,40 @@ router.get('/', (req, res) => {
 
 // Get single user
 router.get('/:id', (req, res) => {
-    const validId = users.some(user => user.id === parseInt(req.params.id));
+    const user = users.filter(user => user.id === parseInt(req.params.id));
 
-    if (validId){
-        res.json(users.filter(user => user.id === parseInt(req.params.id)));
+    if (user.length > 0){
+        res.json(user[0]);
     } else {
-        res.status(400).json({msg: 'user not found'})
+        res.status(404).json({msg: 'user not found'})
     }
 });
 
 
 // create a new user
 router.post('/', (req,res) => {
-    const newUser = {
-        id: uuid.v4(),
-        name: req.body.name,
-        email: req.body.email
+    var re = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
+
+    const emailValidation = re.test(req.body.email);
+
+    if(emailValidation) {
+
+        const newUser = {
+            id: uuid.v4(),
+            name: req.body.name,
+            email: req.body.email
+        }
+
+        if( !newUser.name || !newUser.email){
+            return res.status(400).json({msg : "please enter a name and email"})
+        }
+
+        users.push(newUser);
+        res.send(newUser)
+    } else {
+        res.status(400).json({msg: "please enter valid email"})
     }
 
-    if( !newUser.name || !newUser.email){
-        return res.status(400).json({msg : "please enter a name and email"})
-    }
-
-    users.push(newUser);
-    res.send(users)
 })
 
 
